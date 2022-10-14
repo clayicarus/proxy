14c3a801f104a941c2732034a431286487afca08
clayicarus <1072464850@qq.com>
2022-10-14 17:30:59 +0800

    new

--- tcprelay.cpp
+++ tcprelay.cpp
@@ -3,13 +3,13 @@
 //
 
 #include "tunnel.h"
-
-#include "tunnel.h"
 #include <stdio.h>
 #include <malloc.h>
 #include <sys/resource.h>
 #include <unistd.h>
+#include <algorithm>
 
+using namespace std;
 using namespace muduo;
 using namespace muduo::net;
 
@@ -19,12 +19,43 @@ std::map<string, TunnelPtr> g_tunnels;
 
 void onServerConnection(const TcpConnectionPtr &conn)
 {
-
+    LOG_DEBUG << (conn->connected() ? "UP" : "DOWN");
+    if(conn->connected()) { // connected
+        conn->setTcpNoDelay(true);
+        conn->stopRead();
+        TunnelPtr tunnel(std::make_shared<Tunnel>(g_eventLoop, *g_serverAddr, conn));
+        tunnel->setup();
+        tunnel->connect();
+        g_tunnels[conn->name()] = tunnel;
+    } else {    // disconnected
+        LOG_INFO << conn->name() << " - Source close actively";
+        assert(g_tunnels.find(conn->name()) != g_tunnels.end());
+        g_tunnels[conn->name()]->disconnect();
+        g_tunnels.erase(conn->name());
+    }
 }
 
-void onServerMessage(const TcpConnectionPtr &conn, Buffer *buf, Timestamp)
+void onServerMessage(const TcpConnectionPtr &conn, Buffer *buf, Timestamp time) // only receive requests from source
 {
-
+    LOG_DEBUG << buf->readableBytes();
+    LOG_INFO << conn->name() << " - Request to destination";
+    if(!conn->getContext().empty()) {   // tunnel has connected to destination
+        const auto &sourceConn = boost::any_cast<const TcpConnectionPtr&>(conn->getContext());
+#ifndef NOT_OUTPUT
+        string req_str; // FIXME: O(buf->readableBytes())
+        for_each(buf->toStringPiece().begin(), buf->toStringPiece().end(), [&req_str](const auto &i){
+            if(i >= ' ' && i < 127) {
+                req_str.push_back(i);
+            } else {
+                char format_num[5]; // 0 x 0 0 \0
+                snprintf(format_num, sizeof format_num, "0x%02x", i);
+                req_str.append(string("\\") + format_num);
+            }
+        });
+        fprintf(stderr, "%s : %s - Request\n* begin *\n%s\n* end *\n", time.toFormattedString().c_str(), conn->name().c_str(), req_str.c_str());
+#endif  // NOT_OUTPUT
+        sourceConn->send(buf);  // send requests to destination
+    }
 }
 
 void memstat()
@@ -32,7 +63,39 @@ void memstat()
     malloc_stats();
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    return 0;
+    if(argc < 4) {
+        fprintf(stderr, "Usage: %s <host_ip> <port> <listen_port>\n", argv[0]);
+    } else {
+        LOG_INFO << "pid = " << getpid() << ", tid = " << CurrentThread::tid();
+
+        {
+            // set max virtual memory to 256MB
+            size_t kOneMB = 1024 * 1024;
+            rlimit rl = { 256 * kOneMB, 256 * kOneMB };
+            setrlimit(RLIMIT_AS, &rl);
+        }
+
+        // <host_ip> <port> <listen_port>
+        const char *ip = argv[1];
+        uint16_t port = atoi(argv[2]);
+        InetAddress serverAddr(ip, port);   // destination addr
+        g_serverAddr = &serverAddr;
+        uint16_t acceptPort = atoi(argv[3]);
+        InetAddress listenAddr(acceptPort);
+
+        EventLoop loop;
+        g_eventLoop = &loop;
+        // print memory status every 30 secs
+        // loop.runEvery(30, memstat);
+
+        TcpServer server(&loop, listenAddr, "TcpRelay");
+        server.setConnectionCallback(onServerConnection);
+        server.setMessageCallback(onServerMessage);
+
+        server.start();
+
+        loop.loop();
+    }
 }
--- tunnel.h
+++ tunnel.h
@@ -5,21 +5,27 @@
 #ifndef PROXY_TUNNEL_H
 #define PROXY_TUNNEL_H
 
+#define NOT_OUTPUT
+
 #include <muduo/base/Logging.h>
 #include <muduo/net/EventLoop.h>
 #include <muduo/net/InetAddress.h>
 #include <muduo/net/TcpClient.h>
 #include <muduo/net/TcpServer.h>
 
+#include <algorithm>
+
+// only in tunnel can get response from destination
 class Tunnel : public std::enable_shared_from_this<Tunnel>, muduo::noncopyable {
 static constexpr size_t kHighMark = 1024 * 1024;
 public:
     Tunnel(muduo::net::EventLoop *loop,
            const muduo::net::InetAddress &serverAddr,
-           const muduo::net::TcpConnectionPtr serverConn)
-    : client_(loop, serverAddr, serverConn->name())
+           const muduo::net::TcpConnectionPtr clientConn)
+    : client_(loop, serverAddr/* destination is client */, clientConn->name()),
+      serverConn_(clientConn)   // source conn
     {
-        LOG_INFO << "Tunnel " << serverConn->peerAddress().toIpPort()
+        LOG_INFO << "Tunnel " << clientConn->peerAddress().toIpPort()
                  << " <-> " << serverAddr.toIpPort();
     }
     ~Tunnel()
@@ -33,7 +39,7 @@ public:
         using std::placeholders::_2;
         using std::placeholders::_3;
 
-        client_.setConnectionCallback(std::bind(&Tunnel::onClientConnection/* source connection */,
+        client_.setConnectionCallback(std::bind(&Tunnel::onClientConnection/* destination connection */,
                                                 shared_from_this(), _1));
         client_.setMessageCallback(std::bind(&Tunnel::onClientMessage,
                                              shared_from_this(), _1, _2, _3));
@@ -53,47 +59,62 @@ public:
     }
 
 private:
-    void teardown() // Q3: disconnect destination conn actively
+    void teardown() // Q3: disconnect source conn actively
     {
         client_.setConnectionCallback(muduo::net::defaultConnectionCallback);
         client_.setMessageCallback(muduo::net::defaultMessageCallback);
         if(serverConn_) {
             serverConn_->setContext(boost::any());  // Q2 ?
-            serverConn_->shutdown();    // how about close destination directly ?
+            serverConn_->shutdown();    // how about close source directly ?
         }
         clientConn_.reset();    // free shared_ptr (~clientConn_)
     }
 
-    void onClientConnection(const muduo::net::TcpConnectionPtr &conn)   // source connection
+    void onClientConnection(const muduo::net::TcpConnectionPtr &conn)   // destination connection
     {
         using std::placeholders::_1;
         using std::placeholders::_2;
 
         LOG_DEBUG << (conn->connected() ? "UP" : "DOWN");
-        if(conn->connected()) { // source connected
+        if(conn->connected()) { // destination connected
             conn->setTcpNoDelay(true);
             conn->setHighWaterMarkCallback(std::bind(&Tunnel::onHighWaterMarkWeak,
                                                      std::weak_ptr<Tunnel>(shared_from_this()), kClient, _1, _2),
                                            kHighMark);
             serverConn_->setContext(conn);  // Q2: record conn to match its client_ ?
-            serverConn_->startRead();       // Q1: when source connected then start read destination requests
-            clientConn_ = conn;
-            if(serverConn_->inputBuffer()->readableBytes() > 0) {   // Q1
-                conn->send(serverConn_->inputBuffer()); // send requests from destination to source
+            serverConn_->startRead();       // Q1: when destination connected then start read source requests
+            clientConn_ = conn;             // destination conn
+            if(serverConn_->inputBuffer()->readableBytes() > 0) {   // Q1: not yet connected to destination but got requests from source
+                conn->send(serverConn_->inputBuffer()); // send requests from source to destination
             }
-        } else {    // Q3: source disconnected actively
-            teardown(); // disconnect destination conn actively
+        } else {    // Q3: destination disconnected actively
+            LOG_INFO << conn->name() << " - Source close passively";
+            teardown(); // disconnect source conn actively
         }
     }
 
     void onClientMessage(const muduo::net::TcpConnectionPtr &conn,
                          muduo::net::Buffer *buf,
-                         muduo::Timestamp)  // received from source
+                         muduo::Timestamp time)  // received from destination
     {
         LOG_DEBUG << conn->name() << " " << buf->readableBytes();
         if(serverConn_) {
-            serverConn_->send(buf); // send response from source to destination
-        } else {    // destination died
+#ifndef NOT_OUTPUT
+            muduo::string req_str; // FIXME: O(buf->readableBytes())
+            std::for_each(buf->toStringPiece().begin(), buf->toStringPiece().end(), [&req_str](const auto &i){
+                if(i >= ' ' && i < 127) {
+                    req_str.push_back(i);
+                } else {
+                    char format_num[5]; // 0 x 0 0 \0
+                    snprintf(format_num, sizeof format_num, "0x%02x", i);
+                    req_str.append(muduo::string("\\") + format_num);
+                }
+            });
+            fprintf(stderr, "%s : %s - Response\n* begin *\n%s\n* end *\n", time.toFormattedString().c_str(), conn->name().c_str(), req_str.c_str());
+#endif  // NOT_OUTPUT
+            LOG_INFO << conn->name() << " - Response to source";
+            serverConn_->send(buf); // send response from destination to source
+        } else {    // source died
             buf->retrieveAll(); // discard all received data
             abort();
         }
@@ -112,15 +133,15 @@ private:
         LOG_INFO << (which == kServer ? "server" : "client")
                  << " onHighWaterMark " << conn->name()
                  << " bytes " << bytesToSent;
-        if(which == kServer) {  // destination output buffer full
+        if(which == kServer) {  // source output buffer full
             if(serverConn_->outputBuffer()->readableBytes() > 0) {  // sent not yet
-                clientConn_->stopRead();    // stop reading response from source
+                clientConn_->stopRead();    // stop reading response from destination
                 serverConn_->setWriteCompleteCallback(std::bind(&Tunnel::onWriteCompleteWeak,
                                                                 std::weak_ptr<Tunnel>(shared_from_this()),
-                                                                        kServer, _1));  // continue to send to destination when write completely
+                                                                        kServer, _1));  // continue to send to source when write completely
             }
             // sent yet
-        } else {    // source output buffer full
+        } else {    // destination output buffer full
             if(clientConn_->outputBuffer()->readableBytes() > 0) {
                 serverConn_->stopRead();
                 clientConn_->setWriteCompleteCallback(std::bind(&Tunnel::onWriteCompleteWeak,
@@ -143,8 +164,8 @@ private:
     {
         LOG_INFO << (which == kServer ? "server" : "client")
                  << " onWriteComplete " << conn->name();
-        if(which == kServer) {  // sent to source(server) yet, destination output buffer not full
-            clientConn_->startRead();   // start to read from source
+        if(which == kServer) {  // sent to destination(server) yet, source output buffer not full
+            clientConn_->startRead();   // start to read from destination
             serverConn_->setWriteCompleteCallback(muduo::net::WriteCompleteCallback()); // default callback
         } else {
             serverConn_->startRead();
